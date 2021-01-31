ad9586d08b68a116e2819ac1a90f5a3461cb7ab1
bobexchen <chenxb1992@hotmail.com>
2021-01-31 21:22:19 +0800

    删除之前的配置，用了新配置，但是是要舍弃的方法

--- src/main/java/com/bobexchen/novajavagithub/config/SecurityConfiguration.java
+++ src/main/java/com/bobexchen/novajavagithub/config/SecurityConfiguration.java
@@ -3,58 +3,42 @@ package com.bobexchen.novajavagithub.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.function.Function;
+import java.util.ArrayList;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
     @Override
-    protected UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        String finalPassword = "{bcrypt}" + bCryptPasswordEncoder.encode("123456");
-        manager.createUser(User
-                .withUsername("user_1")
-                .passwordEncoder(function)
-                .password(finalPassword)
-                .authorities("USER")
-                .build());
-        manager.createUser(User
-                .withUsername("user_2")
-                .passwordEncoder(function)
-                .password("{bcrypt}123456")
-                .authorities("USER")
-                .build());
-        return manager;
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("hellxz")
+                .password(passwordEncoder().encode("xyz"))
+                .authorities(new ArrayList<>(0));
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http
-                .requestMatchers().anyRequest()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/oauth/*").permitAll();
-        // @formatter:on
+        //所有请求必须认证
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
     }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-
-    Function<String, String> function = a -> "bcrypt"+a  ;
-
-
 }
