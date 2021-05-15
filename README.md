e17a99dc241eca1ce8af2ae6e6a6c286b3833d08
bobexchen <chenxb1992@hotmail.com>
2021-05-15 17:12:25 +0800

    修改目录

--- README.md
+++
@@ -1,7 +0,0 @@
-# nova-java-github
-
-
-参考文档：
-
-[interest项目](https://github.com/Big-Chinese-Cabbage/interest)
-
--- src/main/java/com/bobexchen/novajavagithub/cache/NovaCache.java
+++
@@ -1,9 +0,0 @@
-package com.bobexchen.novajavagithub.cache;
-
-import com.bobexchen.novajavagithub.model.PersonInfo;
-
-import java.util.Hashtable;
-
-public class NovaCache {
-    public static Hashtable<String, PersonInfo> peopleInfos = new Hashtable<>();
-}
